15728a2466dfe3ee27a2be0ad2e26d359df826ae
Kasia24 <160325853+Kasia24@users.noreply.github.com>
2024-12-01 14:21:09 +0100

    add

--- src/components/redux/store.js
+++ src/components/redux/store.js
@@ -1,25 +1,25 @@
-// src/redux/store.js
+import { configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { combineReducers } from "redux";
+import contactsReducer from "./contactsSlice";
+import filtersReducer from "./filtersSlice";
 
-import { createStore } from "redux";
-import { devToolsEnhancer } from "@redux-devtools/extension";
-
-const initialState = {
-  tasks: [
-    { id: 0, text: "Learn HTML and CSS", completed: true },
-    { id: 1, text: "Get good at JavaScript", completed: true },
-    { id: 2, text: "Master React", completed: false },
-    { id: 3, text: "Discover Redux", completed: false },
-    { id: 4, text: "Build amazing apps", completed: false },
-  ],
-  filters: {
-    status: "all",
-  },
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["contacts"], // Persist only contacts
 };
 
-const rootReducer = (state = initialState, action) => {
-  return state;
-};
+const rootReducer = combineReducers({
+  contacts: contactsReducer,
+  filters: filtersReducer,
+});
 
-// Tworzymy rozszerzenie magazynu, aby dodać DevTools
-const enhancer = devToolsEnhancer();
-export const store = createStore(rootReducer, enhancer);
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+});
+
+export const persistor = persistStore(store);
