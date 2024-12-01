aee4abbca00c5b52ab948af6062aba54f90eb4c7
Kasia24 <160325853+Kasia24@users.noreply.github.com>
2024-12-01 16:11:01 +0100

    add

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { App } from "./components/App";
+import App from "./components/App.jsx";
+import { Provider } from "react-redux";
 import { store } from "..//src/components/redux/store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
