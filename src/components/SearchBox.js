a0bda718c38819caad68a4e62cc3409684147830
Kasia24 <160325853+Kasia24@users.noreply.github.com>
2024-12-01 15:50:27 +0100

    add

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { setFilter } from "../store/filtersSlice";
+import { setFilter } from "..//components/filtersSlice";
 
 const SearchBox = () => {
   const filter = useSelector((state) => state.filters.name);
